4dc489f54df6187bd0c9560ebd27408a3c9121b8
DillonSummers <dsumm11@lsu.edu>
2025-08-12 12:35:17 -0500

    DS commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import logo from './logo.svg';
-import { BrowserTouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <Router>
+    <>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path=":id" />
+          </Routes>
+        </div>
+      </Router>
+
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-           <Route path=":id"></Route>
-        </Routes>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
-    </Router>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    </>
   );
 }
 
